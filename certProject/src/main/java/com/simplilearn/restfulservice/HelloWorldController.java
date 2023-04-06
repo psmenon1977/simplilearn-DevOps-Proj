770559019e59b693ddf65c0e98c6e041c9d44b64
Ubuntu <ubuntu@ip-10-1-1-97.ap-southeast-1.compute.internal>
2023-04-06 15:00:40 +0000

    changed HelloWorldController.java

--- certProject/src/main/java/com/simplilearn/restfulservice/HelloWorldController.java
+++ certProject/src/main/java/com/simplilearn/restfulservice/HelloWorldController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/certProj")
 public class HelloWorldController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello, how are you %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
